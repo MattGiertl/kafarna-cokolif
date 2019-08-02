4db70ba4d5f2e53593be50e705c0f53bdacd1a19
matt.giertl <matus1giertl@gmail.com>
2019-08-03 00:29:18 +0200

    button adjustment

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -2,14 +2,11 @@ import React from "react"
 import styled from "@emotion/styled"
 
 const StyledButton = styled.button({
-  display: "flex",
-  justifyItems: "center",
   backgroundColor: "black",
   color: "white",
   padding: "8px 16px",
-  width: "95px",
+  width: "100px",
   height: "43px",
-  textAlign: "center",
   fontSize: "16px",
   cursor: "pointer",
 })
