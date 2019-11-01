018db438aff821178a344421be772fe804b02113
matt.giertl <matus1giertl@gmail.com>
2019-11-01 14:51:09 +0100

    updated config yml, rmeoved unused sections

--- src/components/sections/AboutSection.js
+++ src/components/sections/AboutSection.js
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "@emotion/styled"
 
-import ReviewBox from "../molecules/ReviewBox"
 import InfoStrip from "../molecules/InfoStrip"
 
 import SectionHeading from "../atoms/SectionHeading"
@@ -17,7 +16,6 @@ const AboutSection = ({ aboutImage, aboutText, review, reviewAuthor }) => (
   <StyledSection id="o-nas">
     <SectionHeading>O KAFÁRNĚ</SectionHeading>
     <Paragraph>{aboutText}</Paragraph>
-    <ReviewBox review={review} author={reviewAuthor} />
     <Image alt="hoši" src={aboutImage} />
     <InfoStrip info="Otervírací doba" infoText="Po - So 8:01 - 19:00" />
     <InfoStrip info="Adresa" infoText="Pavelčákova 5/13, Olomouc, 1. Patro" />
