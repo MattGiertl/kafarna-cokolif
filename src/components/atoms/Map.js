e8bf1f61893ed3ef39778b6f25e870e97537dee8
matt.giertl <matus1giertl@gmail.com>
2019-11-02 11:03:37 +0100

    map added

---
+++ src/components/atoms/Map.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import styled from "@emotion/styled"
+
+const StyledMap = styled.iframe({
+  width: "100%",
+  height: "450px",
+  marginTop: "10px",
+})
+
+const Map = () => (
+  <StyledMap
+    frameBorder="0"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.43086167652!2d19.139850216066684!3d48.716358279273734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47153d53b480f2bf%3A0xf405cc77a8759513!2sMAKRONKOVO%20-%20Cukr%C3%A1rska%20manufakt%C3%BAra!5e0!3m2!1sen!2scz!4v1567684490826!5m2!1sen!2scz"
+  />
+)
+
+export default Map
