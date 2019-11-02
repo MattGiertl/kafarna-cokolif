66e03e769726d4d27ee92f4b3f20b72e485887af
matt.giertl <matus1giertl@gmail.com>
2019-11-02 11:03:48 +0100

    map implemented

--- src/components/sections/VisitUsSection.js
+++ src/components/sections/VisitUsSection.js
@@ -1,26 +1,21 @@
 import React from "react"
 import styled from "@emotion/styled"
 
-
 import Paragraph from "../atoms/Paragraph"
 import SectionHeading from "../atoms/SectionHeading"
-import Image from "../atoms/Image"
+import Map from "../atoms/Map"
 
 const VisitUsSectionWrapper = styled.div({
   display: "flex",
   flexDirection: "column",
 })
 
-const VisitUsSection = ({ visitUsImage }) => (
+const VisitUsSection = () => (
   <VisitUsSectionWrapper id="navstivte-nas">
     <SectionHeading>KDE NÁS MŮŽETE NAJÍT?</SectionHeading>
     <Paragraph margin={0}>Pavelčákova 5/13, Olomouc</Paragraph>
     <Paragraph margin={0}>1. PATRO</Paragraph>
-    <Image
-      alt="kafe"
-      src={visitUsImage}
-      style={{ width: "100%", marginTop: "16px" }}
-    />
+    <Map />
   </VisitUsSectionWrapper>
 )
 
