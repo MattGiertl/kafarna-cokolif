b5b1f60aaf047e3d0d6464d711a755f491ae2208
matt.giertl <matus1giertl@gmail.com>
2019-11-10 17:56:26 +0100

    fluid components

--- src/components/atoms/Twig.js
+++ src/components/atoms/Twig.js
@@ -1,14 +1,45 @@
 import React from "react"
 import styled from "@emotion/styled"
 
-import TwigImg from "../../../static/assets/twig.png"
+// import TwigImg from "../../../static/assets/twig.png"
 
-const StyledTwig = styled.img({
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+
+const StyledTwig = styled(Img)({
   height: "50px",
-  width: "70px",
+  width: "80px",
   margin: "0 auto",
 })
 
-const Twig = () => <StyledTwig src={TwigImg} alt="Cokolif větvička" />
+const Twig = () => {
+  const data = useStaticQuery(twigQuery)
+  const { fluid } = data.file.childImageSharp
+
+  return <StyledTwig fluid={fluid} alt="Cokolif větvička" />
+}
+
+const twigQuery = graphql`
+  query TwigQuery {
+    file(relativePath: { eq: "twig.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 200) {
+          aspectRatio
+          base64
+          originalImg
+          originalName
+          presentationHeight
+          presentationWidth
+          sizes
+          src
+          srcSet
+          srcSetWebp
+          srcWebp
+          tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default Twig
